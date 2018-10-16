d53786fb8305d34e1105814d77f525699f4a9cf8
ashutoshSharma12 <44078050+ashutoshSharma12@users.noreply.github.com>
2018-10-16 19:41:01 +0530

    Add files via upload
    Assignment number 3

---
+++ Pythonfile.py
@@ -0,0 +1,24 @@
+#Answer number1:
+print "Hello World!"  #First Program in Computer Science...
+#Answer number2:
+a = "Acad"
+b = " View"
+print a + b
+#Answer number3:
+x = input("Enter the Value of x = ")
+y = input("Enter the Value of y = ")
+z = input("Enter the Value of z = ")
+print x , y , z
+#Answer number4:
+print "Let's get started"
+#Answer number5:
+s = "Acadview"
+course = "Python"
+fees = 5000
+print '%s %s %d' %(s, course, fees)
+#Answer number6:
+pi = 3.14
+r = input("Enter the Area of the circle")
+Area = pi * r * r
+print Area
+ #Assingment by Ashutosh Sharma.......
